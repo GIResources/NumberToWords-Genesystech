23e579180fad9c3eb7e1617fa609ebbe81ab7ec8
GIResources <86805487+GIResources@users.noreply.github.com>
2022-08-24 10:58:05 +0100

    Add files via upload

---
+++ scriptConverter.php
@@ -0,0 +1,224 @@
+<?php
+
+/*
+if(isset($_POST["submit_btn"])){
+
+    $userInput = $_POST['numInput'];
+
+    $userInput = new userInput("en", userInput::#region);
+    echo $userInput->format(1234);
+}
+*/
+/*
+function numberTowords($num)
+{ 
+$ones = array( 
+0 => " ",    
+1 => "one", 
+2 => "two", 
+3 => "three", 
+4 => "four", 
+5 => "five", 
+6 => "six", 
+7 => "seven", 
+8 => "eight", 
+9 => "nine", 
+10 => "ten", 
+11 => "eleven", 
+12 => "twelve", 
+13 => "thirteen", 
+14 => "fourteen", 
+15 => "fifteen", 
+16 => "sixteen", 
+17 => "seventeen", 
+18 => "eighteen", 
+19 => "nineteen" 
+); 
+$tens = array( 
+1 => "ten",
+2 => "twenty", 
+3 => "thirty", 
+4 => "forty", 
+5 => "fifty", 
+6 => "sixty", 
+7 => "seventy", 
+8 => "eighty", 
+9 => "ninety" 
+); 
+$hundreds = array( 
+"hundred", 
+"thousand", 
+"million", 
+"billion", 
+"trillion", 
+"quadrillion" 
+); //limit t quadrillion 
+$num = number_format($num,2,".",","); 
+$num_arr = explode(".",$num); 
+$wholenum = $num_arr[0]; 
+$decnum = $num_arr[1]; 
+$whole_arr = array_reverse(explode(",",$wholenum)); 
+krsort($whole_arr); 
+$rettxt = ""; 
+foreach($whole_arr as $key => $i){ 
+if($i < 20){ 
+$rettxt .= $ones[$i]; 
+}elseif($i < 100){ 
+$rettxt .= $tens[substr($i,0,1)]; 
+$rettxt .= " ".$ones[substr($i,1,1)]; 
+}else{ 
+$rettxt .= $ones[substr($i,0,1)]." ".$hundreds[0]; 
+$rettxt .= " ".$tens[substr($i,1,1)]; 
+$rettxt .= " ".$ones[substr($i,2,1)]; 
+} 
+if($key > 0){ 
+$rettxt .= " ".$hundreds[$key]." "; 
+} 
+} 
+if($decnum > 0){ 
+$rettxt .= " and "; 
+if($decnum < 20){ 
+$rettxt .= $ones[$decnum]; 
+}elseif($decnum < 100){ 
+$rettxt .= $tens[substr($decnum,0,1)]; 
+$rettxt .= " ".$ones[substr($decnum,1,1)]; 
+} 
+} 
+return $rettxt; 
+} 
+
+extract($_POST);
+if(isset($convert))
+{
+echo "<p align='center' style='color:blue'>".numberTowords("$num")."</p>";
+}
+*/
+
+function convertNumber($num = false)
+{
+    $num = str_replace(array(',', ''), '' , trim($num));
+    if(! $num) {
+        return false;
+    }
+    $num = (int) $num;
+    $words = array();
+    $list1 = array('', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+        'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    );
+    $list2 = array('', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred');
+    $list3 = array('', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion',
+        'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quattuordecillion',
+        'quindecillion', 'sexdecillion', 'septendecillion', 'octodecillion', 'novemdecillion', 'vigintillion'
+    );
+    $num_length = strlen($num);
+    $levels = (int) (($num_length + 2) / 3);
+    $max_length = $levels * 3;
+    $num = substr('00' . $num, -$max_length);
+    $num_levels = str_split($num, 3);
+    for ($i = 0; $i < count($num_levels); $i++) {
+        $levels--;
+        $hundreds = (int) ($num_levels[$i] / 100);
+        $hundreds = ($hundreds ? ' ' . $list1[$hundreds] . ' hundred' . ( $hundreds == 1 ? '' : '' ) . ' ' : '');
+        $tens = (int) ($num_levels[$i] % 100);
+        $singles = '';
+        if ( $tens < 20 ) {
+            $tens = ($tens ? ' and ' . $list1[$tens] . ' ' : '' );
+        } elseif ($tens >= 20) {
+            $tens = (int)($tens / 10);
+            $tens = ' and ' . $list2[$tens] . ' ';
+            $singles = (int) ($num_levels[$i] % 10);
+            $singles = ' ' . $list1[$singles] . ' ';
+        }
+        $words[] = $hundreds . $tens . $singles . ( ( $levels && ( int ) ( $num_levels[$i] ) ) ? ' ' . $list3[$levels] . ' ' : '' );
+    } //end for loop
+    $commas = count($words);
+    if ($commas > 1) {
+        $commas = $commas - 1;
+    }
+    $words = implode(' ',  $words);
+    $words = preg_replace('/^\s\b(and)/', '', $words );
+    $words = trim($words);
+    $words = ucfirst($words);
+    $words = $words . ".";
+    return $words;
+}
+//extract($_POST);
+//if(isset($convert))
+// Function to convert Words to Numbers...
+function word_digit($words) {
+    $warr = explode('',$words);
+    $result = '';
+    foreach($warr as $value){
+        switch(trim($value)){
+            case 'zero':
+                $result .= '0';
+                break;
+            case 'one':
+                $result .= '1';
+                break;
+            case 'two':
+                $result .= '2';
+                break;
+            case 'three':
+                $result .= '3';
+                break;
+            case 'four':
+                $result .= '4';
+                break;
+            case 'five':
+                $result .= '5';
+                break;
+            case 'six':
+                $result .= '6';
+                break;
+            case 'seven':
+                $result .= '7';
+                break;
+            case 'eight':
+                $result .= '8';
+                break;
+            case 'nine':
+                $result .= '9';
+                break;    
+        }
+    }
+    return $result;
+}
+//Reading inputs from the keyboard
+
+if(isset($_POST['convert'])){
+    $toNum = $_POST['num'];
+
+    // $toNum = $toNum;
+    // echo $toNum;
+
+    if(is_numeric($toNum)){
+    $mgs = convertNumber($toNum);
+}else{
+    $mgs = word_digit($toNum);
+}
+
+}
+
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title> Genesystech Test </title>
+</head>
+
+<body style=" text-align: center;">
+<h1>Number Conversion Programme  </h1>
+    
+        <p> This program is developed to help you convert Number (or digits) to Words. <br>
+        This will help you translate any financial numbers into their corresponding values in Words.</p>
+    <hr>
+<?php
+    echo "The Words equivalent of the Numbers you've enter is: <br><b>" . $mgs;
+    
+?>
+    <hr><center> Copyright (c) 2022. Genesys Internship Test. All rights reserved. </center>
+</body>
+    </html>
